2d7b5c579f3a05fc69f83acb7602c2d238c76e9e
hiimkoin <nikoebener@gmail.com>
2017-03-14 16:48:31 +0100

    changed property access

--- src/main/java/de/predic8/routes/HashErrorNotification.java
+++ src/main/java/de/predic8/routes/HashErrorNotification.java
@@ -1,13 +1,12 @@
 package de.predic8.routes;
 
-import de.predic8.util.OAuth;
+import de.predic8.Archive;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class HashErrorNotification extends RouteBuilder {
 
-    public OAuth oAuth = new OAuth();
     private String fileName;
 
     public HashErrorNotification() {
@@ -24,15 +23,15 @@ public class HashErrorNotification extends RouteBuilder {
         from("file:document-archive/logs?fileName=log.txt&noop=true")
                 .log("SENDING HASH ERROR MAIL")
                 .setHeader("subject", simple("Hash Error Detected"))
-                .setHeader("firstName", simple(oAuth.properties.getProperty("user_name")))
+                .setHeader("firstName", simple(Archive.properties.prop.getProperty("user_name")))
                 .setBody(simple(fileName))
                 .to("freemarker:/email-templates/verify_fail.ftl")
                 .to(String.format("smtp://%s?password=%s&username=%s&to=%s&from=%s"
-                        , oAuth.properties.getProperty("email_smtp")
-                        , oAuth.properties.getProperty("email_password")
-                        , oAuth.properties.getProperty("email_username")
-                        , oAuth.properties.getProperty("email_recipient")
-                        , oAuth.properties.getProperty("email_username")));
+                        , Archive.properties.prop.getProperty("email_smtp")
+                        , Archive.properties.prop.getProperty("email_password")
+                        , Archive.properties.prop.getProperty("email_username")
+                        , Archive.properties.prop.getProperty("email_recipient")
+                        , Archive.properties.prop.getProperty("email_username")));
     }
 
     public void start(String fileName) throws Exception {
