ad6c9744ef8e0a1cc25fada0ca5bc744ff5d688e
hiimkoin <nikoebener@gmail.com>
2017-04-20 15:35:52 +0200

    send log.txt as attachment

--- src/main/java/de/predic8/routes/DailyMailNotification.java
+++ src/main/java/de/predic8/routes/DailyMailNotification.java
@@ -1,9 +1,12 @@
 package de.predic8.routes;
 
 import de.predic8.Archive;
+import de.predic8.util.AttachLogfile;
 import de.predic8.util.EmailNewFiles;
 import de.predic8.util.PropertyFile;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -11,18 +14,22 @@ public class DailyMailNotification extends RouteBuilder {
 
     public void configure() throws Exception {
 
+        Endpoint smtp = getContext().getEndpoint(
+                String.format("smtp://%s?password=%s&username=%s&to=%s&from=%s"
+                    , PropertyFile.getInstance().getProperty("email_smtp")
+                    , PropertyFile.getInstance().getProperty("email_password")
+                    , PropertyFile.getInstance().getProperty("email_username")
+                    , PropertyFile.getInstance().getProperty("email_recipient")
+                    , PropertyFile.getInstance().getProperty("email_username")));
+
         from("file:document-archive/notify?fileName=new_files.txt&noop=true").routeId("DailyNotify")
                 .log("SENDING EMAIL")
                 .setHeader("subject", simple("Daily Report"))
                 .setHeader("firstName", simple(PropertyFile.getInstance().getProperty("user_name")))
                 .process(new EmailNewFiles())
                 .to("freemarker:/email-templates/daily_report.ftl")
-                .to(String.format("smtp://%s?password=%s&username=%s&to=%s&from=%s"
-                        , PropertyFile.getInstance().getProperty("email_smtp")
-                        , PropertyFile.getInstance().getProperty("email_password")
-                        , PropertyFile.getInstance().getProperty("email_username")
-                        , PropertyFile.getInstance().getProperty("email_recipient")
-                        , PropertyFile.getInstance().getProperty("email_username")))
+                .process(new AttachLogfile())
+                .to(smtp)
                 .log("EMAIL SEND");
 
     }
