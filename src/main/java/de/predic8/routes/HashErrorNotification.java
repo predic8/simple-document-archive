f6149196ad5c360e8b03a16fa3900e0d77adbc2f
hiimkoin <nikoebener@gmail.com>
2017-04-20 15:46:47 +0200

    attached log.txt

--- src/main/java/de/predic8/routes/HashErrorNotification.java
+++ src/main/java/de/predic8/routes/HashErrorNotification.java
@@ -1,8 +1,10 @@
 package de.predic8.routes;
 
 import de.predic8.Archive;
+import de.predic8.util.AttachLogfile;
 import de.predic8.util.PropertyFile;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -21,18 +23,22 @@ public class HashErrorNotification extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        Endpoint smtp = getContext().getEndpoint(
+                String.format("smtp://%s?password=%s&username=%s&to=%s&from=%s"
+                        , PropertyFile.getInstance().getProperty("email_smtp")
+                        , PropertyFile.getInstance().getProperty("email_password")
+                        , PropertyFile.getInstance().getProperty("email_username")
+                        , PropertyFile.getInstance().getProperty("email_recipient")
+                        , PropertyFile.getInstance().getProperty("email_username")));
+
         from("file:document-archive/logs?fileName=log.txt&noop=true")
                 .log("SENDING HASH ERROR MAIL")
                 .setHeader("subject", simple("Hash Error Detected"))
                 .setHeader("firstName", simple(PropertyFile.getInstance().getProperty("user_name")))
                 .setBody(simple(fileName))
                 .to("freemarker:/email-templates/verify_fail.ftl")
-                .to(String.format("smtp://%s?password=%s&username=%s&to=%s&from=%s"
-                        , PropertyFile.getInstance().getProperty("email_smtp")
-                        , PropertyFile.getInstance().getProperty("email_password")
-                        , PropertyFile.getInstance().getProperty("email_username")
-                        , PropertyFile.getInstance().getProperty("email_recipient")
-                        , PropertyFile.getInstance().getProperty("email_username")))
+                .process(new AttachLogfile())
+                .to(smtp)
                 .log("HASH ERROR MAIL SEND");
     }
 
