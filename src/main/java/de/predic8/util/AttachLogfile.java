9f90bc2214e15f7cc0ab449017bf7ed67d6b5023
hiimkoin <nikoebener@gmail.com>
2017-04-20 15:36:13 +0200

    email attachments

---
+++ src/main/java/de/predic8/util/AttachLogfile.java
@@ -0,0 +1,18 @@
+package de.predic8.util;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultAttachment;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+
+public class AttachLogfile implements Processor{
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        DefaultAttachment attachment = new DefaultAttachment(new FileDataSource("document-archive/logs/log.txt"));
+        exchange.getIn().addAttachmentObject("log.txt", attachment);
+    }
+}
