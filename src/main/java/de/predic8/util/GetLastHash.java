38c664ba917f4f7ae00fc0caf1a7f753ed9267d9
hiimkoin <nikoebener@gmail.com>
2017-06-06 17:48:16 +0200

    minor changes

--- src/main/java/de/predic8/util/GetLastHash.java
+++ src/main/java/de/predic8/util/GetLastHash.java
@@ -5,6 +5,7 @@ import org.apache.camel.Processor;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
@@ -12,6 +13,11 @@ public class GetLastHash implements Processor {
 
     public void process(Exchange exchange) throws Exception {
 
+        String[] lastHashLine = getLastLine().split(" ");
+        exchange.getIn().setBody(lastHashLine[lastHashLine.length - 1]);
+    }
+
+    private String getLastLine() throws IOException {
         String currentLine, lastLine = "";
         try (BufferedReader br = new BufferedReader(new InputStreamReader(
                 new FileInputStream("document-archive/logs/log.txt"), Charset.forName("UTF-8")))) {
@@ -20,8 +26,6 @@ public class GetLastHash implements Processor {
                 lastLine = currentLine;
             }
         }
-
-        String[] lastHash = lastLine.split(" ");
-        exchange.getIn().setBody(lastHash[lastHash.length - 1]);
+        return lastLine;
     }
 }
