c80ea31a128f7e4fb5f228a1559ba15bdd513eef
hiimkoin <nikoebener@gmail.com>
2017-06-06 17:46:30 +0200

    added test cron

--- src/main/java/de/predic8/quartz/DailyMail.java
+++ src/main/java/de/predic8/quartz/DailyMail.java
@@ -8,11 +8,12 @@ public class DailyMail extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("quartz2://notify?cron=0+0+21+?+*+*+*") // at 21:00 every day
+        //from("quartz2://notify?cron=0+*+*+?+*+*+*") // every minute
                 .routeId("Quartz: DailyMail")
                 .log("Start quartz/DailyMail Route")
+                .log("Sending daily mail...")
                 .process(exchange -> {
                     DailyMailNotification notify = new DailyMailNotification();
-                    System.out.println("Sending daily mail...");
                     notify.start();
                 })
                 .log("End quartz/DailyMail Route");
--- src/main/java/de/predic8/quartz/Verify.java
+++ src/main/java/de/predic8/quartz/Verify.java
@@ -8,12 +8,13 @@ public class Verify extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("quartz2://verify?cron=0+0+22+?+*+*+*") // at 22:00 every day
+        //from("quartz2://verify?cron=0+*+*+?+*+*+*") // every minute
                 .routeId("Quartz: Verify")
                 .log("Start quartz/Verify Route")
+                .log("Verify archive...")
                 .process(exchange -> {
                     VerifyRoutes verify = new VerifyRoutes();
                     verify.lastHash = "123";
-                    System.out.println("Verify archive...");
                     verify.start();
                 })
                 .log("End quartz/Verify Route");
