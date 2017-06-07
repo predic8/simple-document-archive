c9a0ac6effbec5783deb4b24a2582f7a08981d5a
hiimkoin <nikoebener@gmail.com>
2017-06-07 14:18:15 +0200

    camel-spring-boot

--- src/main/java/de/predic8/routes/DailyMailNotification.java
+++ src/main/java/de/predic8/routes/DailyMailNotification.java
@@ -1,24 +1,29 @@
 package de.predic8.routes;
 
-import de.predic8.Endpoints;
 import de.predic8.util.AttachLogfile;
 import de.predic8.util.EmailNewFiles;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class DailyMailNotification extends RouteBuilder {
 
+    @Override
     public void configure() throws Exception {
+
+        PropertiesComponent pc = new PropertiesComponent();
+        pc.setLocation("classpath:application.properties");
+        getContext().addComponent("properties", pc);
 
         from("file:document-archive/notify?fileName=new_files.txt&noop=true").routeId("DailyNotify")
                 .log("Sending DailyMail")
                 .setHeader("subject", simple("Daily Report"))
                 .setHeader("firstName", simple("{{user_name}}"))
                 .process(new EmailNewFiles())
-                .to(Endpoints.dailyMailFM)
+                .to("freemarker:/email-templates/daily_report.ftl")
                 .process(new AttachLogfile())
-                .to(Endpoints.smtp)
+                .to("smtp://{{email_smtp}}?password={{email_password}}&username={{email_username}}&to={{email_recipient}}&from={{email_username}}")
                 .log("DailyMail send");
 
     }
