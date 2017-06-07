b0881afdd8fbf406ea28d5cc91100abbb89bfe78
hiimkoin <nikoebener@gmail.com>
2017-06-07 14:16:20 +0200

    camel-spring-boot

--- src/main/java/de/predic8/quartz/Verify.java
+++ src/main/java/de/predic8/quartz/Verify.java
@@ -9,8 +9,8 @@ public class Verify extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        //from("quartz2://verify?cron=0+0+22+?+*+*+*") // at 22:00 every day
-        from("quartz2://verify?cron=0+*+*+?+*+*+*") // every minute
+        from("quartz2://verify?cron=0+0+22+?+*+*+*") // at 22:00 every day
+        //from("quartz2://verify?cron=0+*+*+?+*+*+*") // every minute
                 .routeId("Quartz: Verify")
                 .log("Start quartz/Verify Route")
                 .log("Verify archive...")
