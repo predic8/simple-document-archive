153a7e6ef4098926767d617f4765c1c5e296a6ec
hiimkoin <nikoebener@gmail.com>
2017-06-07 14:17:20 +0200

    begin email testing

---
+++ src/test/java/de/predic8/EMailRoutesTest.java
@@ -0,0 +1,11 @@
+package de.predic8;
+
+import org.apache.camel.test.junit4.CamelTestSupport;
+
+public class EMailRoutesTest extends CamelTestSupport {
+
+    @Override
+    public void setUp() throws Exception {
+
+    }
+}
