56a122e4eb47ddb4a698ca679b58b74b21d87da1
hiimkoin <nikoebener@gmail.com>
2017-06-07 14:15:36 +0200

    camel-spring-boot

--- src/main/java/de/predic8/quartz/DailyMail.java
+++ src/main/java/de/predic8/quartz/DailyMail.java
@@ -2,13 +2,15 @@ package de.predic8.quartz;
 
 import de.predic8.routes.DailyMailNotification;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DailyMail extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("quartz2://notify?cron=0+0+21+?+*+*+*") // at 21:00 every day
-        //from("quartz2://notify?cron=0+*+*+?+*+*+*") // every minute
+        //from("quartz2://notify?cron=0+0+21+?+*+*+*") // at 21:00 every day
+        from("quartz2://notify?cron=0+*+*+?+*+*+*") // every minute
                 .routeId("Quartz: DailyMail")
                 .log("Start quartz/DailyMail Route")
                 .log("Sending daily mail...")
