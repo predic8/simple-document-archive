a88eeac2081f1d6e6acf8c43a97657a0dabbfaf4
hiimkoin <nikoebener@gmail.com>
2017-07-07 18:12:16 +0200

    added searchbar/ load log.txt into mem

--- src/main/java/de/predic8/Archive.java
+++ src/main/java/de/predic8/Archive.java
@@ -1,11 +1,22 @@
 package de.predic8;
 
+import de.predic8.model.ArchivedFile;
+import de.predic8.service.ArchiveService;
 import org.apache.camel.main.Main;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class Archive {
+
+    @Autowired
+    ArchiveService service;
 
     private Main main;
 
@@ -22,5 +33,19 @@ public class Archive {
         //main.addRouteBuilder(new Verify());
         System.out.println("Starting... Use CTRL + C to terminate!");
         main.run();
+    }
+
+    @PostConstruct
+    public void load() throws Exception {
+        try (Stream<String> stream = Files.lines(Paths.get("document-archive/logs/log.txt"))) {
+            stream.forEach(line -> {
+                ArchivedFile archive = new ArchivedFile();
+                archive.setDate(line.split(" ")[0]);
+                archive.setTime(line.split(" ")[1]);
+                archive.setFileName(line.split(" ")[2]);
+                archive.setHash(line.split(" ")[3]);
+                service.archiveFile(archive);
+            });
+        }
     }
 }
