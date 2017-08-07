ab63ad5370a426fca1d2188b6b31bb1c99f4f154
hiimkoin <nikoebener@gmail.com>
2017-08-07 12:35:10 +0200

    added spring security

--- src/main/java/de/predic8/controller/ArchiveController.java
+++ src/main/java/de/predic8/controller/ArchiveController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -15,6 +16,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
+@EnableGlobalMethodSecurity
 public class ArchiveController {
 
     @Autowired
