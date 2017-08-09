572df634744a27b03ffa018a005c9b25aa87dcfe
hiimkoin <nikoebener@gmail.com>
2017-08-09 16:44:52 +0200

    removed imports

--- src/main/java/de/predic8/model/ArchivedFile.java
+++ src/main/java/de/predic8/model/ArchivedFile.java
@@ -1,14 +1,6 @@
 package de.predic8.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Entity
 @Table(name = "Archive")
